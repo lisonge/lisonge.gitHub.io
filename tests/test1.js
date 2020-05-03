42fee387366578b63bee8529520ef78c31992743
Lisonge <lisonge@users.noreply.github.com>
2020-05-03 21:36:15 +0800

    test fetch path

--- tests/test1.js
+++ tests/test1.js
@@ -2,6 +2,12 @@
  * @Date: 2020-05-03 21:29:57
  * @LastEditors: code
  * @Author: code
- * @LastEditTime: 2020-05-03 21:30:14
+ * @LastEditTime: 2020-05-03 21:35:49
  */
 console.log('hello fuck')
+
+async function x1(){
+    const resp = await fetch('./tests/test1.js')
+    console.log(resp)
+}
+x1()
